ec1d131d7a0886f3619f3c8135ace83e100e1885
AkillerA <sshi83684@gmail.com>
2023-01-28 14:10:06 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+from tkinter import *  
+  
+  
+window = Tk()  
+window.title("Добро пожаловать в приложение PythonRu")  
+lbl = Label(window, text="привет желаю тебе хорошего дня😜")  
+lbl.grid(column=0, row=0)  
+window.mainloop()
